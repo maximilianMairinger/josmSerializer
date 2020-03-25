98a81df82ed0d5a516937c1c1bf6b25cf2caa2f3
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-03-25 10:16:14 +0100

    deserialize > parse

--- app/src/josmSerializer.ts
+++ app/src/josmSerializer.ts
@@ -1,5 +1,43 @@
+type Primitive = number | string | boolean | null
+import Xrray from "xrray"
+Xrray(Array)
+
+type JSONObject = {[key: string]: any} | Primitive[]
 
 
-export default function() {
-  
+
+export function serialize(input: JSON | JSONObject) {
+
 }
+
+
+export function deserialize(inp: JSON | JSONObject) {
+  let o = parse(inp)
+
+  
+
+
+  return o
+}
+
+function parse(inp: JSON | JSONObject) {
+  let input = typeof inp === "string" ? JSON.parse(inp) : inp as JSONObject
+  let res = {}
+
+  for (let className in input) {
+    let ar = res[className] = []
+    let length = input[className][Object.keys(input[className]).first].length
+    for (let i = 0; i < length; i++) {
+      ar.add({})
+    }
+
+    for (let propName in input[className]) {
+      for (let i in ar) {
+        ar[i][propName] = input[className][propName][i]
+      }
+    }
+  }
+  
+  return res
+}
+
--- test/src/test.ts
+++ test/src/test.ts
@@ -1,4 +1,39 @@
-import josmSerializer from "./../../app/src/josmSerializer"
-//const testElem = document.querySelector("#test")
+import { deserialize, serialize } from "./../../app/src/josmSerializer"
 
-josmSerializer()
+let me = {
+  students: {
+    name: [
+      "Max",
+      "Daniel"
+    ],
+    subject: [
+      1,
+      1
+    ]
+  },
+  subjects: {
+    color: [
+      1
+    ],
+    students: [
+      [1, 2]
+    ],
+    name: [
+      "MEDt"
+    ]
+  },
+  colors: {
+    r: [
+      255
+    ],
+    g: [
+      255
+    ],
+    b: [
+      255
+    ]
+  }
+}
+
+
+console.log(deserialize(me))
