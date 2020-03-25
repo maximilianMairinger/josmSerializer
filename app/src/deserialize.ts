7ca1f679f4499dedf94668d09eb316650dfe5e54
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-03-25 16:48:54 +0100

    extended Select type & deserialize recursive binding

--- app/src/deserialize.ts
+++ app/src/deserialize.ts
@@ -6,48 +6,100 @@ type GenericDataStore = {
 
 type WhereFunction<PropVal> = (prop: PropVal) => boolean
 type Limit = number
+type Begin = number
+
+type Selector<ClassName, propVal> = ClassName | "length" | "*" | propVal extends number ? ("sum" | "minimum" | "maximum" | "average") : "*"
 
 type Select<DataStore extends GenericDataStore, ClassName extends keyof DataStore, PropName extends keyof DataStore[ClassName], PropVal extends DataStore[ClassName][PropName][number]> = 
 
-"*" | 
-ClassName | 
-ClassName[] | 
+Selector<ClassName, PropVal> |
+Selector<ClassName, PropVal>[] | 
 
-{[className in ClassName]: PropName} | 
-{[className in ClassName]: PropName[]} | 
-{[className in ClassName]: PropName}[] | 
-{[className in ClassName]: PropName[]}[] | 
+{[className in Selector<ClassName, PropVal>]: PropName} | 
+{[className in Selector<ClassName, PropVal>]: PropName[]} | 
+{[className in Selector<ClassName, PropVal>]: PropName}[] | 
+{[className in Selector<ClassName, PropVal>]: PropName[]}[] | 
 
-{[className in ClassName]: {[propName in PropName]: WhereFunction<PropVal>}} | 
-{[className in ClassName]: {[propName in PropName]: WhereFunction<PropVal>}[]} | 
-{[className in ClassName]: {[propName in PropName]: WhereFunction<PropVal>}}[] | 
-{[className in ClassName]: {[propName in PropName]: WhereFunction<PropVal>}[]}[] | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: WhereFunction<PropVal>}} | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: WhereFunction<PropVal>}[]} | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: WhereFunction<PropVal>}}[] | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: WhereFunction<PropVal>}[]}[] | 
 
-{[className in ClassName]: {[propName in PropName]: Limit}} | 
-{[className in ClassName]: {[propName in PropName]: Limit}[]} | 
-{[className in ClassName]: {[propName in PropName]: Limit}}[] | 
-{[className in ClassName]: {[propName in PropName]: Limit}[]}[] | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: Limit}} | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: Limit}[]} | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: Limit}}[] | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: Limit}[]}[] | 
 
-{[className in ClassName]: {[propName in PropName]: {where: WhereFunction<PropVal>, limit: Limit}}} | 
-{[className in ClassName]: {[propName in PropName]: {where: WhereFunction<PropVal>, limit: Limit}}[]} | 
-{[className in ClassName]: {[propName in PropName]: {where: WhereFunction<PropVal>, limit: Limit}}}[] | 
-{[className in ClassName]: {[propName in PropName]: {where: WhereFunction<PropVal>, limit: Limit}}[]}[]
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {where: WhereFunction<PropVal>}}} | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {where: WhereFunction<PropVal>}}[]} | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {where: WhereFunction<PropVal>}}}[] | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {where: WhereFunction<PropVal>}}[]}[] |
 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {limit: Limit}}} | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {limit: Limit}}[]} | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {limit: Limit}}}[] | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {limit: Limit}}[]}[] |
+
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {begin: Begin}}} | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {begin: Begin}}[]} | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {begin: Begin}}}[] | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {begin: Begin}}[]}[] |
+
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {where: WhereFunction<PropVal>, limit: Limit}}} | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {where: WhereFunction<PropVal>, limit: Limit}}[]} | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {where: WhereFunction<PropVal>, limit: Limit}}}[] | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {where: WhereFunction<PropVal>, limit: Limit}}[]}[] |
+
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {where: WhereFunction<PropVal>, limit: Limit, begin: Begin}}} | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {where: WhereFunction<PropVal>, limit: Limit, begin: Begin}}[]} | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {where: WhereFunction<PropVal>, limit: Limit, begin: Begin}}}[] | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {where: WhereFunction<PropVal>, limit: Limit, begin: Begin}}[]}[] |
+
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {limit: Limit, begin: Begin}}} | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {limit: Limit, begin: Begin}}[]} | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {limit: Limit, begin: Begin}}}[] | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {limit: Limit, begin: Begin}}[]}[] |
+
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {where: WhereFunction<PropVal>, begin: Begin}}} | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {where: WhereFunction<PropVal>, begin: Begin}}[]} | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {where: WhereFunction<PropVal>, begin: Begin}}}[] | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {where: WhereFunction<PropVal>, begin: Begin}}[]}[] |
+
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {where: WhereFunction<PropVal>, limit: Limit, begin: Begin}}} | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {where: WhereFunction<PropVal>, limit: Limit, begin: Begin}}[]} | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {where: WhereFunction<PropVal>, limit: Limit, begin: Begin}}}[] | 
+{[className in Selector<ClassName, PropVal>]: {[propName in PropName]: {where: WhereFunction<PropVal>, limit: Limit, begin: Begin}}[]}[]
 
 
 //https://nodejs.org/api/vm.html#vm_vm_runinthiscontext_code_options
 
-export function deserialize<DataStore extends GenericDataStore, ClassName extends keyof DataStore, PropName extends keyof DataStore[ClassName], PropVal extends DataStore[ClassName][PropName][number]>(store: DataStore, select: Select<DataStore, ClassName, PropName, PropVal> = "*") {
+
+type Schema<DataStore extends GenericDataStore, ClassName extends keyof DataStore, PropName extends keyof DataStore[ClassName], PropVal extends DataStore[ClassName][PropName][number]> = {
+  [className in ClassName]: {
+    [propName in PropName]: PropVal
+  }
+}
+
+
+
+
+export function deserialize<DataStore extends GenericDataStore, ClassName extends keyof DataStore, PropName extends keyof DataStore[ClassName], PropVal extends DataStore[ClassName][PropName][number]>(store: DataStore, schema: Schema<DataStore, ClassName, PropName, PropVal>) {
   let data = parse(store)
 
 
+  let classNames = Object.keys(schema)
+
   for (let className in data) {
-    data[className]
+    for (let entry of data[className]) {
+      for (let prop in entry) {
+        if (classNames.includes(entry[prop])) {
+          entry[prop] = data[className][entry[prop]]
+        }
+      }
+    }
   }
   
   
-
-
   return data
 }
 
